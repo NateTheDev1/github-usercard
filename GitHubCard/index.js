a00f92c5dbc0fccbbcd30cbf58d29e04a6c5c7e8
NateTheDev1 <nthnlrichards@gmail.com>
2020-03-17 21:53:19 -0400

    Card Creator Complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+const prefix = "https://api.github.com/users/";
+axios
+  .get(`${prefix}NateTheDev1`)
+  .then(response => {
+    console.log(response);
+    cardCreator(response);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -53,3 +63,66 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+const cardCreator = user => {
+  const gitUser = user.data;
+  let elements = [];
+  let infoElements = [];
+  let cardContainer = document.createElement("div");
+  cardContainer.classList.add("card");
+
+  let picture = document.createElement("img");
+  picture.src = gitUser.avatar_url;
+  elements.push(picture);
+
+  let cardInfo = document.createElement("div");
+  cardInfo.classList.add("card-info");
+
+  let h3 = document.createElement("h3");
+  h3.textContent = gitUser.name;
+  h3.classList.add("name");
+  infoElements.push(h3);
+
+  let username = document.createElement("p");
+  username.textContent = gitUser.login;
+  username.classList.add("username");
+  infoElements.push(username);
+
+  let location = document.createElement("p");
+  location.textContent = gitUser.location;
+  infoElements.push(location);
+
+  let profile = document.createElement("p");
+  profile.textContent = "Profile: ";
+
+  let url = document.createElement("a");
+  url.href = gitUser.url;
+  url.textContent = gitUser.url;
+  profile.appendChild(url);
+  infoElements.push(profile);
+
+  let followers = document.createElement("p");
+  followers.textContent = `Followers: ${gitUser.followers}`;
+  infoElements.push(followers);
+
+  let following = document.createElement("p");
+  following.textContent = `Following: ${gitUser.following}`;
+  infoElements.push(following);
+
+  let bio = document.createElement("p");
+  bio.textContent = `Bio: ${gitUser.bio}`;
+  infoElements.push(bio);
+
+  infoElements.forEach(e => {
+    cardInfo.appendChild(e);
+  });
+
+  elements.push(cardInfo);
+
+  let cards = document.querySelector(".cards");
+  elements.forEach(e => {
+    cardContainer.appendChild(e);
+  });
+
+  cards.appendChild(cardContainer);
+};
